d7c4ef70fa1516a2eacf9f53a961c5a5de52d6c8
Hasa N <112847065+KgHasan@users.noreply.github.com>
2022-09-08 17:04:00 +0600

    Update 4.py

--- 4.py
+++ 4.py
@@ -1316,7 +1316,7 @@ def Subscraption():
 		print("")
 		print(" Copy And Send Key To Admin")
 		print ("")
-		print (" Your Key : "+ak+hasan+key1)
+		print (" Your Key : "+ak+ahsan+key1)
 		print ("")
 		name = input(" Your Name : ")
 		print ("")
